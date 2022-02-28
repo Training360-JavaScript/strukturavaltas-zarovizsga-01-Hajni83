d5e23e117c73f1e3888e3f2aea2dbf77fda34760
Hajni83 <maro.hajnalka@gmail.com>
2022-02-28 15:10:09 +0100

    2/1 rész

--- angular-2/drugstore/src/app/app.component.ts
+++ angular-2/drugstore/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DrugService } from './service/drug.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'drugstore';
+
+  constructor(
+    private drugService:DrugService
+  ){}
+
+  ngOnInit(){
+   this.drugService.getAll().subscribe((drugs)=>{ console.log(drugs)});
+  }
 }
